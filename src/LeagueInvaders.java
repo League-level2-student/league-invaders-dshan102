ebc607d60486cc374501b559a2ac1a2a8cc4e94b
dshan102 <53541391+dshan102@users.noreply.github.com>
2021-01-22 18:32:14 -0800

    LeagueInvaders (IP)

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,16 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+JFrame frame;
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+public static void main(String[] args) {
+	
+}
+public void run() {
+	frame = new JFrame();
+}
+public void setup(int HEIGHT, int WIDTH) {
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 }
