c7c17cb888f2a20dd94868b813cbe0ff134d9c97
dshan102 <53541391+dshan102@users.noreply.github.com>
2021-01-29 18:26:56 -0800

    LeagueInvaders (IP)

---
+++ src/GamePanel.java
@@ -0,0 +1,121 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+	@Override
+	public void paintComponent(Graphics g){
+		if(currentState == MENU){
+		    drawMenuState(g);
+		}else if(currentState == GAME){
+		    drawGameState(g);
+		}else if(currentState == END){
+		    drawEndState(g);
+		}
+	}
+	final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState = MENU;
+    Font titleFont;
+    Font titleFont1;
+	Timer frameDraw;
+	public GamePanel () {
+	    titleFont = new Font("Arial", Font.PLAIN, 48);
+	    titleFont1 = new Font("Arial", Font.PLAIN, 24);
+	    frameDraw = new Timer(1000/60,this);
+	    frameDraw.start();
+	}
+    public void updateMenuState() {  
+    	
+    }
+    public void updateGameState() {  
+    	
+    }
+    public void updateEndState()  { 
+    	
+    }
+    public void drawMenuState(Graphics g) { 
+    	g.setColor(Color.BLUE);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    	g.setFont(titleFont);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("League Invaders", 60, 100);
+    	g.setFont(titleFont1);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("Press ENTER to start", 120, 300);
+    	g.setFont(titleFont1);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("Press SPACE for instructions", 80, 425);
+    	
+    }
+    public void drawGameState(Graphics g) {
+    	g.setColor(Color.BLACK);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    }
+    public void drawEndState(Graphics g)  { 
+    	g.setColor(Color.RED);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(currentState == MENU){
+		    updateMenuState();
+		}else if(currentState == GAME){
+		    updateGameState();
+		}else if(currentState == END){
+		    updateEndState();
+		}
+		repaint();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+		    if (currentState == END) {
+		        currentState = MENU;
+		    } else {
+		        currentState++;
+		    }
+		} 
+		if (e.getKeyCode()==KeyEvent.VK_UP) {
+			if (currentState == GAME) {
+			    System.out.println("UP");
+			}
+		}
+		else if (e.getKeyCode()==KeyEvent.VK_DOWN) {
+			if (currentState == GAME) {
+			    System.out.println("DOWN");
+			}
+		}
+		else if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			if (currentState == GAME) {
+			    System.out.println("LEFT");
+			}
+		}
+		else if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			if (currentState == GAME) {
+			    System.out.println("RIGHT");
+			}
+		}
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,16 +1,30 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
 JFrame frame;
+GamePanel GP;
 public static final int WIDTH = 500;
 public static final int HEIGHT = 800;
 public static void main(String[] args) {
-	
+	LeagueInvaders LI = new LeagueInvaders();
+	LI.setup();
 }
-public void run() {
+
+public void setup() {
 	frame = new JFrame();
-}
-public void setup(int HEIGHT, int WIDTH) {
+	GP = new GamePanel();
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(WIDTH, HEIGHT);
+	frame.add(GP);
+	frame.addKeyListener(GP);
+	frame.setVisible(true);
 }
+
 }
+
+
